d005ee0b9480561e4990872ee28efb8753d3401f
Chris Williams <chrisleewilliams.84@gmail.com>
2023-09-08 16:33:00 -0500

    rendered test data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
-    def index 
+  def index
+    companies = %w[Apple Microsoft Greenhouse Sony Toshiba Samsung Nike Reebok]
+
+    @company_list = []
+
+    companies.each do |company|
+      name = company
+      price = rand(100)
+
+      company_hash = {}
+      company_hash[:name] = name
+      company_hash[:price] = price
+      @company_list << company_hash
     end
+  end
 end
