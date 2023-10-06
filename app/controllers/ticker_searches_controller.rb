b9c1490d60e894e5dfb9b2775e63795ced1aeaac
Chris Williams <chrisleewilliams.84@gmail.com>
2023-10-06 13:42:52 -0500

    add search bar

---
+++ app/controllers/ticker_searches_controller.rb
@@ -0,0 +1,5 @@
+class TickerSearchesController < ActionController::Base
+  def show
+    # redirect_to :show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#index"
+  root 'application#index'
+  get '/ticker_searches', to: 'ticker_searches#show'
 end
